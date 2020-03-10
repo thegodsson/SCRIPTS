4ae72ba0c7055af60ed0a48da28d63b150dccd65
thegodsson <61194538+thegodsson@users.noreply.github.com>
2020-03-10 11:36:09 +0100

    Add files via upload

---
+++ tabassoc_v3.ps1
@@ -0,0 +1,28 @@
+﻿
+
+
+$fichier = Get-Content -Path C:\Users\jmdfrancfort\Documents\POWERSHELL\datas.txt
+
+$(foreach ($line in $fichier) {
+    
+    $ville = $line.split("|")[2]
+    $tab_ville = @()
+
+        foreach($i in $ville){        
+            $tab_ville += $i
+            }
+          
+         
+            
+        foreach($i in $tab_ville) {        
+            $resultat = ($fichier | Select-String -Pattern $i -AllMatches).Matches.Count            
+            $final = $i + " ==> " +  $resultat
+            $final
+
+            }
+      
+    
+  
+  #}) | Sort-Object| Get-Unique
+  }) | Get-Unique
+  #})
